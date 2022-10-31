44f1da1fe1635a113543e6643ac6cca2039918e9
hapchoke <80566116+hapchoke@users.noreply.github.com>
2022-10-31 22:04:48 +0900

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+export default App;
 
 function App() {
+  const [textList, setTextList] = useState([]);
+
+  // stateを変える関数　　addとdelete
+  function addState(text){
+    setTextList((textList)=>{
+        return [...textList,text];
+    });
+  }
+  function deleteState(index){
+    setTextList((textList)=>{
+      const newTextList = [...textList];
+      newTextList.splice(index,1);
+      return newTextList;
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <From addState={addState}/>
+        <ContentList textList={textList} deleteState={deleteState}/>
     </div>
   );
 }
 
-export default App;
+// 入力欄と追加ボタン
+function From(props){
+  const [text, setText] = useState("");
+
+  const add_text = ()=>{
+    if(text!=''){
+      props.addState(text);
+      setText('');
+    }
+  }
+  return(
+    <div className='add-text-form'>
+      <input className='input-text' value={text} onChange={(e)=>setText(e.target.value)}></input>
+      <button className='add-button' onClick={add_text}>追加</button>
+    </div>
+  )
+}
+
+
+function ContentList(props){
+  return(
+    <div className='content-list'>
+      {props.textList.map((text,index)=>
+        <li className='content' key={text+index}>
+          <Content text={text} id={index} deleteState={props.deleteState}/>
+        </li>
+      )}
+    </div>
+  )
+}
+
+
+function Content(props){
+  return(
+    <>
+      <button className='content-button' onClick={()=>{props.deleteState(props.id)}}>X</button>
+      <div className='content-text-area'>
+        <div className='content-text'>
+          {props.text}
+        </div>
+      </div>
+    </>
+  )
+}
